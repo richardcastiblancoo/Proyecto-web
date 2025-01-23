09ca6bb57bc8377aa8c06c98f591689258d9cf05
richardcastiblancoo <castiblancorichard8@email.com>
2025-01-23 03:13:23 -0500

    agregando mi nuevo proyecto parte 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap()],
+});
